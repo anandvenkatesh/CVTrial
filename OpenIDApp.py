d6c2d60cbf7643eef7ec601dd830ea3e68b19a0b
Anand <Anand.venkatesh@hotmail.com>
2019-05-22 14:10:21 -0700

    Modified to Helloworld string

--- OpenIDApp.py
+++ OpenIDApp.py
@@ -1,13 +1,6 @@
-from flask import Flask, render_template
-
+from flask import Flask
 app = Flask(__name__)
 
-@app.route('/')
-def index():
-    return "Index Page"
-@app.route('/main')
-def main():
-    return render_template('main.html')
-
-if __name__=="__main__":
-    app.run()
+@app.route("/")
+def hello():
+    return "Hello World!"
